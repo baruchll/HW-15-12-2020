58cbbf1e5015a122d1b1283923098d4095c745ef
Baruch <benmlehrfeld@gmail.com>
2020-12-15 20:50:55 +0200

    Add files via upload

---
+++ Employee.java
@@ -0,0 +1,23 @@
+package com.company;
+/*
+public class Employee {
+    static int numOfEmployees = 0;
+        String name;
+        int salary;
+        double seniority;
+
+
+        public Employee(String name, int salary, double seniority) {
+            this.name = name;
+            this.salary = salary;
+            this.seniority = seniority;
+
+        }
+
+    //qusetion 1
+    public  String[] method2() {
+            while(int i = 0; i < numOfEmployees; i++);
+        if (seniority>= 120 && salary< 15000);
+    }
+}
+*/
